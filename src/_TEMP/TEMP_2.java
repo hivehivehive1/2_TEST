f7c8814559ddb3387e5980f436bc54f7caee3037
OutStandING <hive@usa.com>
2019-05-28 01:12:33 -0400

    a

--- src/_TEMP/TEMP_2.java
+++ src/_TEMP/TEMP_2.java
@@ -2,5 +2,19 @@ package _TEMP;
 import java.util.*;
 public class TEMP_2 {
 	public static void main(String[] args) {
-	
-}}
+
+		
+String str = "a";
+
+char c1 = str.charAt(0); //c1='a';
+char c2 = str.charAt( -1);
+
+		
+
+		 if(str.length() <= 1){
+		 System.out.println(str);
+		 }else{
+
+		  System.out.println(str.substring(0, str.length() - 2) + c1 + c2);
+		 }
+		}}
